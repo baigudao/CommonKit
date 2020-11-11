ec7fb4fbc2b6cb4fd1a6a946215eefa38663235e
lihuailong <lhl@cn.gree.com>
2020-11-11 16:53:17 +0800

    libs_util

---
+++ libs_util/src/main/java/com/common/util/IConfig.kt
@@ -0,0 +1,13 @@
+package com.common.util
+
+/**
+ * @author admin
+ * @data 2018/4/25
+ * @describe
+ */
+
+interface IConfig {
+    companion object {
+        const val SHARED_PREFERENCES_FILE_NAME = "Shared_Preferences_FILE_NAME"
+    }
+}
