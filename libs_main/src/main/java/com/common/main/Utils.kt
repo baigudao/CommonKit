2a475bd0ca6df669e251cebc4b42a4656435ab0f
lihuailong <lhl@cn.gree.com>
2020-11-11 16:19:09 +0800

    test

---
+++ libs_main/src/main/java/com/common/main/Utils.kt
@@ -0,0 +1,14 @@
+package com.devin.commonkit
+
+import android.content.Context
+import android.widget.Toast
+
+/**
+ * Created by lhl on 2020/11/11.
+ */
+object Utils {
+
+    fun takeToast(context: Context, str: String) {
+        Toast.makeText(context, str, Toast.LENGTH_SHORT).show()
+    }
+}
